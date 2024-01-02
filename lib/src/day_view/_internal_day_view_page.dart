1282e269d8bf8aac77da89acc140d7ba753975b6
Prabin Lamsal <prabin.lamsal@ebpearls.com>
2024-01-02 09:35:02 +0545

    initial MIT lisenced Open Source code

---
+++ lib/src/day_view/_internal_day_view_page.dart
@@ -0,0 +1,270 @@
+// Copyright (c) 2021 Simform Solutions. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+import '../components/_internal_components.dart';
+import '../components/event_scroll_notifier.dart';
+import '../enumerations.dart';
+import '../event_arrangers/event_arrangers.dart';
+import '../event_controller.dart';
+import '../modals.dart';
+import '../painters.dart';
+import '../typedefs.dart';
+
+/// Defines a single day page.
+class InternalDayViewPage<T extends Object?> extends StatelessWidget {
+  /// Width of the page
+  final double width;
+
+  /// Height of the page.
+  final double height;
+
+  /// Date for which we are displaying page.
+  final DateTime date;
+
+  /// A builder that returns a widget to show event on screen.
+  final EventTileBuilder<T> eventTileBuilder;
+
+  /// Controller for calendar
+  final EventController<T> controller;
+
+  /// A builder that builds time line.
+  final DateWidgetBuilder timeLineBuilder;
+
+  /// Builds custom PressDetector widget
+  final DetectorBuilder dayDetectorBuilder;
+
+  /// Settings for hour indicator lines.
+  final HourIndicatorSettings hourIndicatorSettings;
+
+  /// Custom painter for hour line.
+  final CustomHourLinePainter hourLinePainter;
+
+  /// Flag to display live time indicator.
+  /// If true then indicator will be displayed else not.
+  final bool showLiveLine;
+
+  /// Settings for live time indicator.
+  final HourIndicatorSettings liveTimeIndicatorSettings;
+
+  /// Height occupied by one minute of time span.
+  final double heightPerMinute;
+
+  /// Width of time line.
+  final double timeLineWidth;
+
+  /// Offset for time line widgets.
+  final double timeLineOffset;
+
+  /// Height occupied by one hour of time span.
+  final double hourHeight;
+
+  /// event arranger to arrange events.
+  final EventArranger<T> eventArranger;
+
+  /// Flag to display vertical line.
+  final bool showVerticalLine;
+
+  /// Offset  of vertical line.
+  final double verticalLineOffset;
+
+  /// Called when user taps on event tile.
+  final CellTapCallback<T>? onTileTap;
+
+  /// Called when user long press on calendar.
+  final DatePressCallback? onDateLongPress;
+
+  /// Called when user taps on day view page.
+  ///
+  /// This callback will have a date parameter which
+  /// will provide the time span on which user has tapped.
+  ///
+  /// Ex, User Taps on Date page with date 11/01/2022 and time span is 1PM to 2PM.
+  /// then DateTime object will be  DateTime(2022,01,11,1,0)
+  final DateTapCallback? onDateTap;
+
+  /// Defines size of the slots that provides long press callback on area
+  /// where events are not there.
+  final MinuteSlotSize minuteSlotSize;
+
+  /// Notifies if there is any event that needs to be visible instantly.
+  final EventScrollConfiguration scrollNotifier;
+
+  /// Display full day events.
+  final FullDayEventBuilder<T> fullDayEventBuilder;
+
+  /// Flag to display half hours.
+  final bool showHalfHours;
+
+  /// Flag to display quarter hours.
+  final bool showQuarterHours;
+
+  /// Settings for half hour indicator lines.
+  final HourIndicatorSettings halfHourIndicatorSettings;
+
+  /// Settings for half hour indicator lines.
+  final HourIndicatorSettings quarterHourIndicatorSettings;
+
+  final ScrollController scrollController;
+
+  /// Emulate vertical line offset from hour line starts.
+  final double emulateVerticalOffsetBy;
+
+  /// Defines a single day page.
+  const InternalDayViewPage({
+    Key? key,
+    required this.showVerticalLine,
+    required this.width,
+    required this.date,
+    required this.eventTileBuilder,
+    required this.controller,
+    required this.timeLineBuilder,
+    required this.hourIndicatorSettings,
+    required this.hourLinePainter,
+    required this.showLiveLine,
+    required this.liveTimeIndicatorSettings,
+    required this.heightPerMinute,
+    required this.timeLineWidth,
+    required this.timeLineOffset,
+    required this.height,
+    required this.hourHeight,
+    required this.eventArranger,
+    required this.verticalLineOffset,
+    required this.onTileTap,
+    required this.onDateLongPress,
+    required this.onDateTap,
+    required this.minuteSlotSize,
+    required this.scrollNotifier,
+    required this.fullDayEventBuilder,
+    required this.scrollController,
+    required this.dayDetectorBuilder,
+    required this.showHalfHours,
+    required this.showQuarterHours,
+    required this.halfHourIndicatorSettings,
+    required this.quarterHourIndicatorSettings,
+    required this.emulateVerticalOffsetBy,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final fullDayEventList = controller.getFullDayEvent(date);
+    return Container(
+      height: height,
+      width: width,
+      child: Column(
+        children: [
+          fullDayEventList.isEmpty
+              ? SizedBox.shrink()
+              : fullDayEventBuilder(fullDayEventList, date),
+          Expanded(
+            child: SingleChildScrollView(
+              controller: scrollController,
+              child: SizedBox(
+                height: height,
+                width: width,
+                child: Stack(
+                  children: [
+                    CustomPaint(
+                      size: Size(width, height),
+                      painter: hourLinePainter(
+                        hourIndicatorSettings.color,
+                        hourIndicatorSettings.height,
+                        timeLineWidth + hourIndicatorSettings.offset,
+                        heightPerMinute,
+                        showVerticalLine,
+                        verticalLineOffset,
+                        hourIndicatorSettings.lineStyle,
+                        hourIndicatorSettings.dashWidth,
+                        hourIndicatorSettings.dashSpaceWidth,
+                        emulateVerticalOffsetBy,
+                      ),
+                    ),
+                    if (showHalfHours)
+                      CustomPaint(
+                        size: Size(width, height),
+                        painter: HalfHourLinePainter(
+                          lineColor: halfHourIndicatorSettings.color,
+                          lineHeight: halfHourIndicatorSettings.height,
+                          offset:
+                              timeLineWidth + halfHourIndicatorSettings.offset,
+                          minuteHeight: heightPerMinute,
+                          lineStyle: halfHourIndicatorSettings.lineStyle,
+                          dashWidth: halfHourIndicatorSettings.dashWidth,
+                          dashSpaceWidth:
+                              halfHourIndicatorSettings.dashSpaceWidth,
+                        ),
+                      ),
+                    if (showQuarterHours)
+                      CustomPaint(
+                        size: Size(width, height),
+                        painter: QuarterHourLinePainter(
+                          lineColor: quarterHourIndicatorSettings.color,
+                          lineHeight: quarterHourIndicatorSettings.height,
+                          offset: timeLineWidth +
+                              quarterHourIndicatorSettings.offset,
+                          minuteHeight: heightPerMinute,
+                          lineStyle: quarterHourIndicatorSettings.lineStyle,
+                          dashWidth: quarterHourIndicatorSettings.dashWidth,
+                          dashSpaceWidth:
+                              quarterHourIndicatorSettings.dashSpaceWidth,
+                        ),
+                      ),
+                    dayDetectorBuilder(
+                      width: width,
+                      height: height,
+                      heightPerMinute: heightPerMinute,
+                      date: date,
+                      minuteSlotSize: minuteSlotSize,
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: EventGenerator<T>(
+                        height: height,
+                        date: date,
+                        onTileTap: onTileTap,
+                        eventArranger: eventArranger,
+                        events: controller.getEventsOnDay(
+                          date,
+                          includeFullDayEvents: false,
+                        ),
+                        heightPerMinute: heightPerMinute,
+                        eventTileBuilder: eventTileBuilder,
+                        scrollNotifier: scrollNotifier,
+                        width: width -
+                            timeLineWidth -
+                            hourIndicatorSettings.offset -
+                            verticalLineOffset,
+                      ),
+                    ),
+                    TimeLine(
+                      height: height,
+                      hourHeight: hourHeight,
+                      timeLineBuilder: timeLineBuilder,
+                      timeLineOffset: timeLineOffset,
+                      timeLineWidth: timeLineWidth,
+                      showHalfHours: showHalfHours,
+                      showQuarterHours: showQuarterHours,
+                      key: ValueKey(heightPerMinute),
+                    ),
+                    if (showLiveLine && liveTimeIndicatorSettings.height > 0)
+                      IgnorePointer(
+                        child: LiveTimeIndicator(
+                          liveTimeIndicatorSettings: liveTimeIndicatorSettings,
+                          width: width,
+                          height: height,
+                          heightPerMinute: heightPerMinute,
+                          timeLineWidth: timeLineWidth,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/painters.dart
@@ -0,0 +1,284 @@
+// Copyright (c) 2021 Simform Solutions. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+import 'enumerations.dart';
+
+/// Paints 24 hour lines.
+class HourLinePainter extends CustomPainter {
+  /// Color of hour line
+  final Color lineColor;
+
+  /// Height of hour line
+  final double lineHeight;
+
+  /// Offset of hour line from left.
+  final double offset;
+
+  /// Height occupied by one minute of time stamp.
+  final double minuteHeight;
+
+  /// Flag to display vertical line at left or not.
+  final bool showVerticalLine;
+
+  /// left offset of vertical line.
+  final double verticalLineOffset;
+
+  /// Style of the hour and vertical line
+  final LineStyle lineStyle;
+
+  /// Line dash width when using the [LineStyle.dashed] style
+  final double dashWidth;
+
+  /// Line dash space width when using the [LineStyle.dashed] style
+  final double dashSpaceWidth;
+
+  /// Emulates offset of vertical line from hour line starts.
+  final double emulateVerticalOffsetBy;
+
+  /// Paints 24 hour lines.
+  HourLinePainter({
+    required this.lineColor,
+    required this.lineHeight,
+    required this.minuteHeight,
+    required this.offset,
+    required this.showVerticalLine,
+    required this.emulateVerticalOffsetBy,
+    this.verticalLineOffset = 10,
+    this.lineStyle = LineStyle.solid,
+    this.dashWidth = 4,
+    this.dashSpaceWidth = 4,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final dx = offset + emulateVerticalOffsetBy;
+    final paint = Paint()
+      ..color = lineColor
+      ..strokeWidth = lineHeight;
+
+    for (var i = 1; i < Constants.hoursADay; i++) {
+      final dy = i * minuteHeight * 60;
+      if (lineStyle == LineStyle.dashed) {
+        var startX = dx;
+        while (startX < size.width) {
+          canvas.drawLine(
+              Offset(startX, dy), Offset(startX + dashWidth, dy), paint);
+          startX += dashWidth + dashSpaceWidth;
+        }
+      } else {
+        canvas.drawLine(Offset(dx, dy), Offset(size.width, dy), paint);
+      }
+    }
+
+    if (showVerticalLine) if (lineStyle == LineStyle.dashed) {
+      var startY = 0.0;
+      while (startY < size.height) {
+        canvas.drawLine(Offset(offset + verticalLineOffset, startY),
+            Offset(offset + verticalLineOffset, startY + dashWidth), paint);
+        startY += dashWidth + dashSpaceWidth;
+      }
+    } else {
+      canvas.drawLine(Offset(offset + verticalLineOffset, 0),
+          Offset(offset + verticalLineOffset, size.height), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate is HourLinePainter &&
+        (oldDelegate.lineColor != lineColor ||
+            oldDelegate.offset != offset ||
+            lineHeight != oldDelegate.lineHeight ||
+            minuteHeight != oldDelegate.minuteHeight ||
+            showVerticalLine != oldDelegate.showVerticalLine);
+  }
+}
+
+class HalfHourLinePainter extends CustomPainter {
+  /// Color of half hour line
+  final Color lineColor;
+
+  /// Height of half hour line
+  final double lineHeight;
+
+  /// Offset of half hour line from left.
+  final double offset;
+
+  /// Height occupied by one minute of time stamp.
+  final double minuteHeight;
+
+  /// Style of the half hour line
+  final LineStyle lineStyle;
+
+  /// Line dash width when using the [LineStyle.dashed] style
+  final double dashWidth;
+
+  /// Line dash space width when using the [LineStyle.dashed] style
+  final double dashSpaceWidth;
+
+  /// Paint half hour lines
+  HalfHourLinePainter({
+    required this.lineColor,
+    required this.lineHeight,
+    required this.offset,
+    required this.minuteHeight,
+    required this.lineStyle,
+    this.dashWidth = 4,
+    this.dashSpaceWidth = 4,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = lineColor
+      ..strokeWidth = lineHeight;
+
+    for (var i = 0; i < Constants.hoursADay; i++) {
+      final dy = i * minuteHeight * 60 + (minuteHeight * 30);
+      if (lineStyle == LineStyle.dashed) {
+        var startX = offset;
+        while (startX < size.width) {
+          canvas.drawLine(
+              Offset(startX, dy), Offset(startX + dashWidth, dy), paint);
+          startX += dashWidth + dashSpaceWidth;
+        }
+      } else {
+        canvas.drawLine(Offset(offset, dy), Offset(size.width, dy), paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate is HourLinePainter &&
+        (oldDelegate.lineColor != lineColor ||
+            oldDelegate.offset != offset ||
+            lineHeight != oldDelegate.lineHeight ||
+            minuteHeight != oldDelegate.minuteHeight);
+  }
+}
+
+//using HalfHourIndicatorSettings for this too
+class QuarterHourLinePainter extends CustomPainter {
+  /// Color of quarter hour line
+  final Color lineColor;
+
+  /// Height of quarter hour line
+  final double lineHeight;
+
+  /// Offset of quarter hour line from left.
+  final double offset;
+
+  /// Height occupied by one minute of time stamp.
+  final double minuteHeight;
+
+  /// Style of the quarter hour line
+  final LineStyle lineStyle;
+
+  /// Line dash width when using the [LineStyle.dashed] style
+  final double dashWidth;
+
+  /// Line dash space width when using the [LineStyle.dashed] style
+  final double dashSpaceWidth;
+
+  /// Paint quarter hour lines
+  QuarterHourLinePainter({
+    required this.lineColor,
+    required this.lineHeight,
+    required this.offset,
+    required this.minuteHeight,
+    required this.lineStyle,
+    this.dashWidth = 4,
+    this.dashSpaceWidth = 4,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = lineColor
+      ..strokeWidth = lineHeight;
+
+    for (var i = 0; i < Constants.hoursADay; i++) {
+      final dy1 = i * minuteHeight * 60 + (minuteHeight * 15);
+      final dy2 = i * minuteHeight * 60 + (minuteHeight * 45);
+
+      if (lineStyle == LineStyle.dashed) {
+        var startX = offset;
+        while (startX < size.width) {
+          canvas.drawLine(
+              Offset(startX, dy1), Offset(startX + dashWidth, dy1), paint);
+          startX += dashWidth + dashSpaceWidth;
+
+          canvas.drawLine(
+              Offset(startX, dy2), Offset(startX + dashWidth, dy2), paint);
+          startX += dashWidth + dashSpaceWidth;
+        }
+      } else {
+        canvas.drawLine(Offset(offset, dy1), Offset(size.width, dy1), paint);
+        canvas.drawLine(Offset(offset, dy2), Offset(size.width, dy2), paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate is HourLinePainter &&
+        (oldDelegate.lineColor != lineColor ||
+            oldDelegate.offset != offset ||
+            lineHeight != oldDelegate.lineHeight ||
+            minuteHeight != oldDelegate.minuteHeight);
+  }
+}
+
+/// Paints a single horizontal line at [offset].
+class CurrentTimeLinePainter extends CustomPainter {
+  /// Color of time indicator.
+  final Color color;
+
+  /// Height of time indicator.
+  final double height;
+
+  /// offset of time indicator.
+  final Offset offset;
+
+  /// Flag to show bullet at left side or not.
+  final bool showBullet;
+
+  /// Radius of bullet.
+  final double bulletRadius;
+
+  /// Paints a single horizontal line at [offset].
+  CurrentTimeLinePainter({
+    this.showBullet = true,
+    required this.color,
+    required this.height,
+    required this.offset,
+    this.bulletRadius = 5,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawLine(
+      Offset(offset.dx, offset.dy),
+      Offset(size.width, offset.dy),
+      Paint()
+        ..color = color
+        ..strokeWidth = height,
+    );
+
+    if (showBullet)
+      canvas.drawCircle(
+          Offset(offset.dx, offset.dy), bulletRadius, Paint()..color = color);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) =>
+      oldDelegate is CurrentTimeLinePainter &&
+      (color != oldDelegate.color ||
+          height != oldDelegate.height ||
+          offset != oldDelegate.offset);
+}
